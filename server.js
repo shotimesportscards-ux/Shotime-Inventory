eb51b38e4065f2babfeb9d0d097e7eef7e843895
shotimesportscards-ux <shotimesportscards@gmail.com>
2026-04-20 13:43:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
